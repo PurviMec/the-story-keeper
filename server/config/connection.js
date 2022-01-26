822a9983a5103f183566deaf4ab1196e0dcbe3fb
PurviMec <princy.mecwan94@gmail.com>
2022-01-26 17:20:39 -0500

    add connection.js

---
+++ server/config/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/the_story_keeper', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+});
+
+module.exports = mongoose.connection;
