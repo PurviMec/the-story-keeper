9cf3f17b3633c70bf5e17ed691a48fe6e77f82d6
PurviMec <princy.mecwan94@gmail.com>
2022-01-31 11:06:31 -0500

    login signup auth.js added

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Auth from '../../utils/auth';
 
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <nav className="text-center">
+        <nav className="text-center m-4">
             <Link to="/">
               <h1>THE STORY KEEPER</h1>
             </Link>
+            { Auth.loggedIn() ? (
+              <>
+                <Link to="/profile">Profile</Link>
+              </>
+            )}
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
             <Link to="/profile">Profile</Link>
+            <Link to="/">LogOut</Link>
             Search
         </nav>
       </div>
