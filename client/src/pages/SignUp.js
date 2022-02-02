32117f742a379463070bb334aaa8a15e75755a68
PurviMec <princy.mecwan94@gmail.com>
2022-02-02 12:35:33 -0500

    commit

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -6,35 +6,59 @@ import Auth from '../utils/auth';
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
   const [addUser, { error }] = useMutation(ADD_USER);
-
   // update state based on form input changes
-  const handleChange = event => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
 
     setFormState({
       ...formState,
-      [name]: value
+      [name]: value,
     });
   };
 
   // submit form
-  const handleFormSubmit = async event => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-    console.log(formState); 
-    
-    // use try/catch instead of promises to handle errors
+
     try {
-      // execute addUser mutation and pass in variable data from form
       const { data } = await addUser({
-        variables: formState 
+        variables: { ...formState },
       });
 
-      console.log(data, 'sldfjl');
       Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
-  };  
+  };
+
+  // update state based on form input changes
+  // const handleChange = event => {
+  //   const { name, value } = event.target;
+
+  //   setFormState({
+  //     ...formState,
+  //     [name]: value
+  //   });
+  // };
+
+  // // submit form
+  // const handleFormSubmit = async event => {
+  //   event.preventDefault();
+  //   console.log(formState); 
+    
+  //   // use try/catch instead of promises to handle errors
+  //   try {
+  //     // execute addUser mutation and pass in variable data from form
+  //     const { data } = await addUser({
+  //       variables: {...formState }
+  //     });
+
+  //     console.log(data, 'sldfjl');
+  //     Auth.login(data.addUser.token);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };  
 
   return (
     <main className="flex-row justify-center mb-4">
